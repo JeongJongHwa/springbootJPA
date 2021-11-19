c5bad2da6b622347675dbdfe8487e1d5cfc87dcf
JJH <jjh_8416@irskorea.co.kr>
2021-11-19 16:48:58 +0900

    no message

--- src/main/java/com/study/a01/controller/BoardController.java
+++ src/main/java/com/study/a01/controller/BoardController.java
@@ -2,9 +2,6 @@ package com.study.a01.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.a01.domain.Board;
